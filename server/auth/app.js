1e27fcc1c3e327c3ad06c1c8f4a4062b75456f5f
Oluwajolasun Jaiyesimi <51948882+oluwajolasun@users.noreply.github.com>
2023-07-29 23:58:24 -0600

    still working on env files

--- server/auth/app.js
+++ server/auth/app.js
@@ -18,8 +18,8 @@ var cookieParser = require("cookie-parser");
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri =
-  process.env.BACKEND_URI + "/callback" || "http://localhost:8888/callback"; // Your redirect uri
-var frontend_uri = process.env.FRONTEND_URI; // Your redirect uri
+  process.env.BACKEND_URI || "https://auth-spotify-api.onrender.com/callback"; // Your redirect uri
+var frontend_uri = process.env.FRONTEND_URI || "http://localhost:5173"; // Your frontend uri
 const port = process.env.PORT || 8888;
 
 /**
