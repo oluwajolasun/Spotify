769d2502a7b972afd8aaa9fb5c5cbd9dec123e88
Oluwajolasun Jaiyesimi <51948882+oluwajolasun@users.noreply.github.com>
2024-09-11 05:05:44 -0600

    fixed scopes in backend

--- server/auth/app.js
+++ server/auth/app.js
@@ -53,8 +53,7 @@ app.get("/login", function (req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope =
-    "user-read-private user-read-email user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative";
+  var scope = "user-top-read user-follow-read playlist-read-collaborative";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
